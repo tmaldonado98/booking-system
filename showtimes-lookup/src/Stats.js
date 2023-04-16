bab54490f850e97167a83a8b271825976fb5f48e
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-04-16 13:35:08 -0700

    Hooked up images, and summary. Now will try to hook up salary information + cost of living.

--- showtimes-lookup/src/Stats.js
+++ showtimes-lookup/src/Stats.js
@@ -1,8 +1,8 @@
 import { Heading, Text, Input, Button } from '@chakra-ui/react';
-// import { css } from '@emotion/react'
 import { Progress } from 'reactstrap';
-
+import { Helmet } from 'react-helmet';
 import axios from 'axios';
+
 import { useEffect, useState } from 'react';
 
 import './stats.css';
@@ -11,6 +11,7 @@ export default function Stats(props) {
     const [slug, setSlug] = useState(null);
     const [scores, setScores] = useState(null);
     const [show, setShow] = useState(false);
+    const [imgUrl, setImgUrl] = useState('');
     // console.log(props.data['_links']['city:urban_area']['href'])
 
     function setScoreFromSlug(){
@@ -23,47 +24,50 @@ export default function Stats(props) {
     }
 
     useEffect(() => {
+        if (!props.data._links['city:urban_area']) {
+            setSlug(null)
+        } else{
         axios.get(`${props.data['_links']['city:urban_area']['href']}`,
         {headers: {'Content-Type': 'application/json'}})
         .then(response => {
             setSlug(response.data)
             // console.log(response.data)
         })
-        // .then(setScoreFromSlug)
         .catch(error => console.log(error))
+        }
+        
+        document.cookie = "SameSite=None; Secure";
 
     }, [])
 
     useEffect(() => {
+        ///This ensure that score is set only after slug has been set. 
+        ///If I use .then, or even useEffect, they do not wait until axios response.
         if (slug !== null) {
             setScoreFromSlug();
-            increase();
+            if (slug === null) {
+                return false;
+            } else{
+            axios.get(`${slug._links['ua:images']['href']}`,
+            {headers: {'Content-Type': 'application/json'}})
+            .then(response => {
+                setImgUrl(response.data)
+            });
+            }
           }
     }, [slug])
-
-//// bars animation JS
-function increase() {
-    // Change the variable to modify the speed of the number increasing from 0 to (ms)
-    let SPEED = 40;
-    // Retrieve the percentage value
-    let limit = parseInt(document.getElementsByTagName("span").innerHTML, 10);
-
-    for(let i = 0; i <= limit; i++) {
-        setTimeout(function () {
-           document.getElementsByClassName("number").innerHTML = i + "%";
-        }, SPEED * i);
-
-    }
-}
-
 
 
     return (
         <>
             <Heading style={{textAlign: 'center'}} as='h4'>{props.data.full_name}</Heading>
+            <div id='img-w-summary'>
+                {imgUrl && <img src={imgUrl.photos[0].image.web} />}
+                {scores && <p dangerouslySetInnerHTML={{ __html: scores.summary}} id='summary'></p>}            </div>
+            
             <div className="chart">
                 {/* <h4>Population: {(props.data.population).toLocaleString('us')}</h4> */}
-                <div style={{opacity: 0}}><p>0</p><span>00</span><Progress value={0}/></div>
+                <div className='individual-divs' style={{opacity: 0}}><p style={{margin:0}}>0<span>00</span></p><Progress value={0}/></div>
 
                 {scores !== null &&
                         scores['categories'].map(each => {
@@ -82,10 +86,8 @@ function increase() {
 
                               return (
                             <div className='individual-divs'>
-                                <p style={{fontWeight: 'bold'}}>{each.name} </p>
-                                    {/* <!-- Change the value below --> */}
-                                    <span className='number'>{(Number(each.score_out_of_10) * 10).toFixed(1)}%</span>
-                                    {/* <div css={css`@keyframes load {0% {width: 0;} 100% {width: ${barWidth}%}}`} className="bar"></div> */}
+                                <p style={{fontWeight: 'bold'}}>{each.name} - <span className='number'>{(Number(each.score_out_of_10) * 10).toFixed(1)}%</span></p>
+                                    
                                     <Progress
                                         animated
                                         color={level}
@@ -94,10 +96,16 @@ function increase() {
                                         />
                             </div>
                             )
-                        })
-                        
-                    
+                        })                       
                 }
+                {/* {slug && 
+                <div id='widget'>
+                    <a className="teleport-widget-link" href={`https://teleport.org/cities/${slug.slug}/`}>Cost of living - {slug.name}</a>
+                    <Helmet>
+                        <script async class="teleport-widget-script" data-url={`https://teleport.org/cities/${slug.slug}/widget/costs/?currency=USD`} data-max-width="770" data-height="955" src="https://teleport.org/assets/firefly/widget-snippet.min.js"></script>
+                    </Helmet>
+                </div>
+                } */}
             </div>
         </>
     );
